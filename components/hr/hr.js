8ca673c1453222fc1fc766585cef1886f5a5ea4c
jungleford <jungleford@gmail.com>
2021-10-27 11:06:56 +0800

    分隔线单独做一个组件。从“对折问题”小程序拷贝而来。

---
+++ components/hr/hr.js
@@ -0,0 +1,20 @@
+// components/hr/hr.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    width: {
+      type: String,
+      value: '100%'
+    },
+    size: {
+      type: String,
+      value: '2rpx'
+    },
+    color: {
+      type: String,
+      value: '#d8d8d8'
+    }
+  },
+})
